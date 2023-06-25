1efcf7bf57d40d93883a1978cb32de4230c9da4f
molotovisq <josephgdroid@gmail.com>
2023-06-24 22:58:53 -0300

    register and login working

--- database/migrations/2023_06_25_013358_create_books_table.php
+++ database/migrations/2023_06_25_013358_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('isbn');
+            $table->double('value');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
-
+  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+  
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+  
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  
+  
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+        
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
 });
